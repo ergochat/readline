1e5f674c7ea9ce73dcb150d9ef11e23a546b5a75
Shivaram Lingamneni <slingamn@cs.stanford.edu>
2023-03-12 22:00:33 -0400

    hide Debug function

--- utils.go
+++ utils.go
@@ -298,22 +298,17 @@ func (r *rawModeHandler) Exit() error {
 
 // -----------------------------------------------------------------------------
 
-func sleep(n int) {
-	Debug(n)
-	time.Sleep(2000 * time.Millisecond)
-}
-
 // print a linked list to Debug()
 func debugList(l *list.List) {
 	idx := 0
 	for e := l.Front(); e != nil; e = e.Next() {
-		Debug(idx, fmt.Sprintf("%+v", e.Value))
+		debugPrint(idx, fmt.Sprintf("%+v", e.Value))
 		idx++
 	}
 }
 
 // append log info to another file
-func Debug(o ...interface{}) {
+func debugPrint(o ...interface{}) {
 	f, _ := os.OpenFile("debug.tmp", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	fmt.Fprintln(f, o...)
 	f.Close()
