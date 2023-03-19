ee3111db8a9c3da50b29d0a82263cd1100a89163
Shivaram Lingamneni <slingamn@cs.stanford.edu>
2023-03-19 01:58:13 -0400

    term: don't unset OPOST when entering raw mode
    See https://github.com/ergochat/readline/issues/8

--- internal/term/term_unix.go
+++ internal/term/term_unix.go
@@ -31,7 +31,7 @@ func makeRaw(fd int) (*State, error) {
 	// This attempts to replicate the behaviour documented for cfmakeraw in
 	// the termios(3) manpage.
 	termios.Iflag &^= unix.IGNBRK | unix.BRKINT | unix.PARMRK | unix.ISTRIP | unix.INLCR | unix.IGNCR | unix.ICRNL | unix.IXON
-	termios.Oflag &^= unix.OPOST
+	//termios.Oflag &^= unix.OPOST
 	termios.Lflag &^= unix.ECHO | unix.ECHONL | unix.ICANON | unix.ISIG | unix.IEXTEN
 	termios.Cflag &^= unix.CSIZE | unix.PARENB
 	termios.Cflag |= unix.CS8
