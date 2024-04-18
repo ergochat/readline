98a12f896b830b2e0591d69651c470959dcd7885
Shivaram Lingamneni <slingamn@cs.stanford.edu>
2024-04-17 22:33:52 -0400

    don't allocate to read ANSI sequences

--- readline_test.go
+++ readline_test.go
@@ -25,3 +25,44 @@ func TestRace(t *testing.T) {
 
 	rl.Readline()
 }
+
+func TestParseCPRResponse(t *testing.T) {
+	badResponses := []string{
+		"",
+		";",
+		"\x00",
+		"\x00;",
+		";\x00",
+		"x",
+		"1;a",
+		"a;1",
+		"a;1;",
+		"1;1;",
+		"1;1;1",
+	}
+	for _, response := range badResponses {
+		if _, err := parseCPRResponse([]byte(response)); err == nil {
+			t.Fatalf("expected parsing of `%s` to fail, but did not", response)
+		}
+	}
+
+	goodResponses := []struct {
+		input  string
+		output cursorPosition
+	}{
+		{"1;2", cursorPosition{1, 2}},
+		{"0;2", cursorPosition{0, 2}},
+		{"0;0", cursorPosition{0, 0}},
+		{"48378;9999999", cursorPosition{48378, 9999999}},
+	}
+
+	for _, response := range goodResponses {
+		got, err := parseCPRResponse([]byte(response.input))
+		if err != nil {
+			t.Fatalf("could not parse `%s`: %v", response.input, err)
+		}
+		if got != response.output {
+			t.Fatalf("expected %s to parse to %#v, got %#v", response.input, response.output, got)
+		}
+	}
+}
